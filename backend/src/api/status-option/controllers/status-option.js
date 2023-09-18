f0b55337f7a158c46d28e435e4faca9f1feee651
Petr <piotr.makarov@gmail.com>
2023-09-18 12:54:23 +0500

    Полный перенос оплаты на сторону сервера

--- backend/src/api/status-option/controllers/status-option.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * status-option controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::status-option.status-option');
