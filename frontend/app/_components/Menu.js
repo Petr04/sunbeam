9a73deb29dfc9e13932de0fc465d2a2afb91f11d
vikikrivich <levigowik@gmail.com>
2023-09-21 19:24:54 +0600

    strict mobile version of mainPage

--- frontend/app/_components/Menu.js
+++ frontend/app/_components/Menu.js
@@ -7,17 +7,24 @@ export default function Menu() {
   const toggleModal = () => setShowModal(!showModal)
 
   return (
-    <div className="pt-[50px] px-[68px]  md:px-[30px]">
+    <div className="pt-[50px] px-[68px] md:px-[30px]">
       <nav className="flex justify-between items-center z-0">
-        <a href="#"><img src="/logo_yellow.svg" alt="logo" className="w-[276px]"></img></a>
+        <a href="#">
+          <img 
+            src="/logo_yellow.svg" alt="logo" 
+            className="wide:w-[276px] xl:w-[276px] md:w-[14rem]"/>
+        </a>
 
         <ul className="flex gap-8 wide:gap-12  lg:hidden md:hidden">
-          <li><a href="" className="font-light text-xl wide:text-2xl">расписание</a></li>
-          <li><a href="" className="font-light text-xl wide:text-2xl">новости</a></li>
-          <li><a href="" className="font-light text-xl wide:text-2xl">наши работы</a></li>
-          <li><a href="" className="font-light text-xl wide:text-2xl">арт-лавка</a></li>
-          <li><a href="" className="font-light text-xl wide:text-2xl">контакты</a></li>
-          <li><a href="" className="font-light text-xl wide:text-2xl">ru</a><img src="/ru-en_yellow.svg" alt="ru-en" className="inline mx-1" ></img></li>
+          <li><a href="/schedule" className="font-light text-xl wide:text-2xl">расписание</a></li>
+          <li><a href="/news" className="font-light text-xl wide:text-2xl">новости</a></li>
+          <li><a href="/our-works" className="font-light text-xl wide:text-2xl">наши работы</a></li>
+          <li><a href="/art-shop" className="font-light text-xl wide:text-2xl">арт-лавка</a></li>
+          <li><a href="/contacts" className="font-light text-xl wide:text-2xl">контакты</a></li>
+          <li>
+            <a href="" className="font-light text-xl wide:text-2xl">ru</a>
+            <img src="/ru-en_yellow.svg" alt="ru-en" className="inline mx-1"/>
+          </li>
         </ul>
 
         <button
@@ -29,7 +36,11 @@ export default function Menu() {
       </nav>
 
       {showModal === true &&
-        <div className="absolute md:top-[120px] md:left-5 lg:top-[140px] lg:left-10 bg-yellow_bg z-0"> 
+        <div className="
+          absolute bg-yellow_bg z-0
+          md:top-[120px] md:left-5 
+          lg:top-[140px] lg:left-10
+          "> 
           <ul className="grid grid-cols-2 gap-2">
             <li><a href="/schedule" className="font-light md:text-xl md:mx-5 lg:text-2xl lg:mx-8">расписание</a></li>
             <li><a href="" className="font-light md:text-xl md:mx-5 lg:text-2xl lg:mx-8">новости</a></li>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,21 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    // screens: {
+    //   sm: {'max': '500px'},
+    //   md: {'max': '600px'},
+    //   lg: {'min': '601px', 'max': '1180px'},
+    //   lt: {'min': '1181px', 'max': '1440px'},
+    //   xl: {'min': '1181px', 'max': '1600px'},
+    //   wide: {'min': '1601px', 'max': '3000px'}
+    // },
     screens: {
-      sm: {'max': '400px'},
+      wide: {'max': '3000px'},
+      xl: {'max': '1600px'},
+      lt: {'max': '1440px'},
+      lg: {'max': '1180px'},
       md: {'max': '600px'},
-      lg: {'min': '601px', 'max': '1180px'},
-      lt: {'min': '1181px', 'max': '1440px'},
-      xl: {'min': '1181px', 'max': '1600px'},
-      wide: {'min': '1601px', 'max': '3000px'}
+      sm: {'max': '500px'},
     },
     container: {
       padding: '20px',
