437ccc1e784658f48ab4c8e72cd9386ad6a83c48
Petr <piotr.makarov@gmail.com>
2023-10-05 16:29:25 +0500

    Fixed bugs

--- frontend/components/Dialog.js
+++ frontend/components/Dialog.js
@@ -8,22 +8,15 @@ import { useRouter } from 'next/navigation'
 function Dialog({ closeUrl, children }, ref) {
   const router = useRouter()
   const dialog = useRef(null)
-  useEffect(() => {
-    dialog.current.close()
-    dialog.current.showModal()
-  }, [])
+  useEffect(() => {dialog.current.showModal()}, [])
 
-  const closeDialog = useCallback(() => {
-    dialog.current.close()
-    setTimeout(() => router.push(closeUrl, { scroll: false }), 300)
-  }, [])
-
-  useImperativeHandle(ref, () => ({ closeDialog }))
+  useImperativeHandle(ref, () => ({ closeDialog: () => dialog.current.close() }))
   
   return (
     <dialog
       ref={dialog}
       className="bg-gray-03 rounded-[40px] text-gray-02 z-50 w-[580px] shadow-md backdrop:bg-dim max-w-full"
+      onClose={() => setTimeout(() => router.push(closeUrl, { scroll: false }), 300)}
     >
       <div
         onClick={e => {
