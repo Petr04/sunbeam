d3974f64f69ca654e550dc19bd283b902b853659
vikikrivich <levigowik@gmail.com>
2023-10-06 07:02:52 +0600

    fixing merge conflicts

--- frontend/app/news/_components/ItemOfListNews.js
+++ frontend/app/news/_components/ItemOfListNews.js
@@ -61,6 +61,8 @@
 // }
 
 import Image from 'next/image'
+import Layout from '@/components/Layout'
+import ky from '@/ky'
 
 function numeralOfAge(age) {
   if (age % 10 == 1) return 'год'
@@ -72,25 +74,25 @@ export default function News({ title, description, image, author, technology }) 
   const path = process.env.NEXT_PUBLIC_API_URL + image.url;
 
   return (
-    <div suppressHydrationWarning={true} className="flex flex-col gap-[20px] p-[20px] bg-gray-03 rounded-[35px]">
-      <Image
-        alt="Работа"
-        src={path}
-        width={343}
-        height={343}
-        className="w-full h-auto rounded-[15px]"
-      />
-      <div className="inline-flex w-fit gap-[10px] px-[12px] py-[6px] bg-gray-04 rounded-full text-[16px]">
-        <Image src="/badge-bullet-grey-02.svg" width={6} height={6} alt="-" />
-        {text}
+      <div suppressHydrationWarning={true} className="flex flex-col gap-[20px] p-[20px] bg-gray-03 rounded-[35px]">
+        <Image
+          alt="Работа"
+          src={path}
+          width={343}
+          height={343}
+          className="w-full h-auto rounded-[15px]"
+        />
+        <div className="inline-flex w-fit gap-[10px] px-[12px] py-[6px] bg-gray-04 rounded-full text-[16px]">
+          <Image src="/badge-bullet-grey-02.svg" width={6} height={6} alt="-" />
+          {text}
+          </div>
+        <div className="flex flex-col gap-[10px]">
+          <h2 className="text-[36px] font-semibold mt-[-20px] mb-[-13px]">{title}</h2>
+          <p className="text-[21px]">{author.full_name}, {author.age} {numeralOfAge(author.age)}</p>
         </div>
-      <div className="flex flex-col gap-[10px]">
-        <h2 className="text-[36px] font-semibold mt-[-20px] mb-[-13px]">{title}</h2>
-        <p className="text-[21px]">{author.full_name}, {author.age} {numeralOfAge(author.age)}</p>
+        <button className="rounded-[17px] py-[13px] bg-gray-04 font-semibold text-[17px] shadow-md">
+          Купить
+        </button>
       </div>
-      <button className="rounded-[17px] py-[13px] bg-gray-04 font-semibold text-[17px] shadow-md">
-        Купить
-      </button>
-    </div>
   )
 }
--- frontend/app/news/page.js
+++ frontend/app/news/page.js
@@ -1,25 +1,27 @@
-import ky from 'ky'
-import useMemo from 'react'
+import Layout from '@/components/Layout'
+import ky from '@/ky'
 
 export default async function News() {
   const news = await ky.get('api/news?populate[0]=image&populate[1]=author').json()
 
   return (
-    <main className="wide:w-[80%] xl:w-[100%] lg:w-[80%] md:w-[100%] mx-auto grid grid-cols-1 h-fit items-center z-0">
+    <Layout>
+      <main className="wide:w-[80%] xl:w-[100%] lg:w-[80%] md:w-[100%] mx-auto grid grid-cols-1 h-fit items-center z-0">
 
-      <div className="flex gap-[8rem] mx-auto mt-[3rem]">
-        <div className="
-          text-[120px] font-bold
-          ">Новости</div>
-        <img 
-          src="/news/longSpiral.svg" alt="longSpiral"
-          className="
-          "
-          />
-        <ListNews news={news} />
+        <div className="flex gap-[8rem] mx-auto mt-[3rem]">
+          <div className="
+            text-[120px] font-bold
+            ">Новости</div>
+          <img 
+            src="/news/longSpiral.svg" alt="longSpiral"
+            className="
+            "
+            />
+          <ListNews news={news} />
 
-      </div>
-      
-    </main>
+        </div>
+        
+      </main>
+    </Layout>
   )
 }
